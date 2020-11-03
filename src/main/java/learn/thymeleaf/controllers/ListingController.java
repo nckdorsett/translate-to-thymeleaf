7cb3f52b4543e487ca45484ecb36da4924ec767a
jamie <jamielepito@gmail.com>
2020-11-03 14:09:46 -0600

    added listings and controllers

---
+++ src/main/java/learn/thymeleaf/controllers/ListingController.java
@@ -0,0 +1,15 @@
+package learn.thymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ListingController {
+
+    @GetMapping("/listings")
+    public String getListingsHome(Model model){
+        model.addAttribute("listings");
+        return "";
+    }
+}
